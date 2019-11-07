7060e6a213547508a28d5753ff06d3896351eb54
JonathonMSmith <36175570+JonathonMSmith@users.noreply.github.com>
2019-11-07 12:54:43 -0500

    added delete lines function

--- image_ruiner.py
+++ image_ruiner.py
@@ -1,6 +1,6 @@
 import shutil
 
-def ruin_image(filename, gap=1, skip=300):
+def delete_middle(filename, gap=1, skip=300):
     split = filename.split('.')
     file_copy = split[0] + '_' + str(gap) + '_' + str(skip) + '_copy.' + split[1]
     with open(file_copy, 'wb') as new_file:
@@ -13,3 +13,17 @@ def ruin_image(filename, gap=1, skip=300):
                     first_half = line[:half]
                     second_gap_half = line[half+gap:]
                     new_file.write(first_half+second_gap_half)
+
+def delete_lines(filename, start=0, stop=None):
+    split = filename.split('.')
+    file_copy = split[0] + '_' + str(start) + '_' + str(stop) + '_copy.' + split[1]
+    if stop == None:
+        stop = len(open(filename, 'rb').readlines())
+        print(stop)
+    with open(file_copy, 'wb') as new_file:
+        with open(filename, 'rb') as old_file:
+            for i, line in enumerate(old_file):
+                if i < start or i > stop:
+                    new_file.write(line)
+                else:
+                    continue
